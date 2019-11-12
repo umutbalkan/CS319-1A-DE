e9096876592abb57e95440584f84ae84015aa61f
grkngs <gurkan.gur@ug.bilkent.edu.tr>
2019-11-13 01:16:27 +0200

    Comments and changed var names

--- src/io/Score.java
+++ src/io/Score.java
@@ -1,18 +1,25 @@
 package io;
-
+/*
+ * Public Class of Score
+ * 
+ * */
 public class Score{
-private String name;
-private int score;
-
+  //Variables
+  private String name;
+  private int score;
+  
+  //Constructor
   public Score(String nam, int scor){
     name = nam;
     score = scor;
   }
   
+  //Get method of score
   public int getScore(){
     return this.score;
   }
   
+  //Get methode of name
   public String getName(){
     return this.name;
   }
--- src/io/Score.java~
+++ src/io/Score.java~
@@ -1,5 +1,19 @@
 package io;
 
-public class Score {
+public class Score{
+private String name;
+private int score;
 
+  public Score(String nam, int scor){
+    name = nam;
+    score = scor;
+  }
+  
+  public int getScore(){
+    return this.score;
+  }
+  
+  public String getName(){
+    return this.name;
+  }
 }
