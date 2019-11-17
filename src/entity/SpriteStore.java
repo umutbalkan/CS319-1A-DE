b2415dab744a46efb6741014ce890eaa2398f4c0
grkngs <gurkan.gur@ug.bilkent.edu.tr>
2019-11-17 23:05:30 +0200

    SpriteStore

--- src/entity/SpriteStore.java
+++ src/entity/SpriteStore.java
@@ -1,5 +1,26 @@
 package entity;
-
+import java.util.HashMap<K,V>;
+/*
+ * Public class of SpriteStore
+ * */
 public class SpriteStore {
 
+    private static SpriteStore sSoleInstance = new SpriteStore();
+    
+    private HashMap sprites;
+    private Sprite sprite;
+    //private constructor.
+    private SpriteStore(){}
+
+    public static SpriteStore getInstance() {
+        return sSoleInstance;
+    }
+    
+    public Sprite getSprite(String ref){
+      
+    }
+    
+    private void fail(String msg){
+      
+    }
 }
