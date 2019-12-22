6cb0714db6136e959a874ec33625d4215c65a462
grkngs <gurkan.gur@ug.bilkent.edu.tr>
2019-12-22 21:31:41 +0300

    update

--- src/logic/GameEngine.java
+++ src/logic/GameEngine.java
@@ -34,6 +34,7 @@ import entity.GameObject;
 import entity.GiantM;
 import entity.Lander;
 import entity.MotherShip;
+import entity.Mutant;
 import entity.Ship;
 import entity.Swarmer;
 import io.HighScoreManager;
@@ -56,6 +57,7 @@ public class GameEngine{
 	private LinkedList<Lander> landerList;
 	private LinkedList<Bullet> enemyBulletList;
 	private LinkedList<Swarmer> swarmerList;
+	private LinkedList<Mutant> mutantList;
 	private MotherShip pod1;
 	private MotherShip pod2;
 	private GiantM boss1;
@@ -92,6 +94,8 @@ public class GameEngine{
 	}
 	
 	private void initGameObject() {
+		//Create lists, ship, wave conditions
+		
 		ship = new Ship(40,40);
 		gamePane.getChildren().add(ship.getImageView());
 		shipBulletList = new LinkedList<Bullet>();
@@ -118,19 +122,24 @@ public class GameEngine{
 		landerList = new LinkedList<Lander>();
 		enemyBulletList = new LinkedList<Bullet>();
 		swarmerList = new LinkedList<Swarmer>();
+		mutantList = new LinkedList<Mutant>();
 		boo=1;
 		baiterNumber = 5;
 		con=0;
 	}
 	
 	private void processGame() {
+		//Process method of the game
 		if(ship.getNumberOfLives()!=0) {
+		//MoveShip and FireBullet runs in every wave
 		moveShip();
 		fireBullet();
 		
+		//Start first wave
 		initWave1();
+		//Start second wave
 		initWave2();
-		initWave3();
+		initWave3();  //To start from a wave, set waveNumber to its wave, and make comment the upper init methods
 		initWave4();
 		initWave5();
 		
@@ -139,43 +148,46 @@ public class GameEngine{
 		}
 	
 
-	private void fireBullet() {
-		if(inputManager.getBullet()) {
+	private void fireBullet() { 
+		//Conditions of bullet fire
+		if(inputManager.getBullet()) { //Get pressed
 			a = 0;
 		}
-		if(a==0 && !inputManager.getBullet()) {
+		if(a==0 && !inputManager.getBullet()) { //Create bullet when button released
+			//Bullet created when button is released, because game loop runs faster than key listener. Every time button pressed, this loop runs 4-5 times.
+			//To block that bullet setted on releasing the button
 			a = 1;
 			shipBulletList.add(new Bullet(ship.getX(),ship.getY(),ship.getDirection(),1));
 			gamePane.getChildren().add(shipBulletList.get(shipBulletList.size()-1).getImageView());
 		}
 		
-		for(int i=0; i<shipBulletList.size(); i++) {
+		for(int i=0; i<shipBulletList.size(); i++) { //Move the active bullets
 			shipBulletList.get(i).move(3, 0);
 			shipBulletList.get(i).countTimer();
-			if(shipBulletList.get(i).getTimer() == 100) {
+			if(shipBulletList.get(i).getTimer() == 100) { //Delete the bullet if it goes too much
 				removeGameObject(shipBulletList.get(i));
 				shipBulletList.remove(i);
 			}
 		}
 	}
 	
-	private void initWave1(){
-		if(waveNumber==1) {
+	private void initWave1(){ //Wave 1
+		if(waveNumber==1) { //Set objects specific to wave 1
 			wave1Set();
 			waveNumber=-1;
 			wave1 = true;
 		}
-		if(wave1 == true) {
+		if(wave1 == true) { //Start process when setting is done
 			processWave1();
 		}
 		
-		if(wave1 == true && landerList.size()==0) {
+		if(wave1 == true && landerList.size()==0) { //Finish first wave when all landers are dead
 			long currentTime = (System.currentTimeMillis()/1000);
 			if(b==1) {
 			finishTime = (System.currentTimeMillis()/1000);
 			b = 0;
 			}
-			if(currentTime-finishTime==5) {
+			if(currentTime-finishTime==5) { //Wait five seconds and start wave1 final boss
 			wave1 = false;
 			wave1Final = true;
 			boss1 = new GiantM(ship.getX()+400,ship.getY());
@@ -186,15 +198,15 @@ public class GameEngine{
 		
 		
 		if(wave1Final) {
-			if(boss1.getLife()<0) {
+			if(boss1.getLife()<0) { //Wave1Final ending conditions
 				wave1Final=false;
 				
-				waveNumber = 2;
+				waveNumber = 2; //Pass to other wave
 				gamePane.getChildren().remove(boss1.getImageView());
 				boss1 = null;
 			}
 			else {
-				processWave1Final();
+				processWave1Final(); //Process wave1Final
 			}
 		}
 	}
@@ -409,24 +421,29 @@ public class GameEngine{
 	private void moveLanders() {
 		for(int i=0; i<landerList.size(); i++) {
 			if(landerList.get(i).getHasAstronaut()==false) {
+			// If a lander collides with an astronaut, it abducts it
 			if(collisionManager.isCollide(landerList.get(i),astranoutList.get(i)))
 				landerList.get(i).abductAstronaut(astranoutList.get(i));
 			}
-			landerList.get(i).controlAstronaut();
-			if(landerList.get(i).getY()==650)
+			landerList.get(i).controlAstronaut(); //If the lander hasnt got an astronaut, it can not control it. Specified inside control method
+			
+			//Prevent getting out of map
+			if(landerList.get(i).getY()==650) 
 			landerList.get(i).setDirection(-1);
 			if(landerList.get(i).getY()==200)
 				landerList.get(i).setDirection(1);
 			
-				landerList.get(i).move();
+			//Move the lander
+			landerList.get(i).move();
 			}
 	}
 	
 	private void moveAst() {
 		for(int i=0; i<astranoutList.size(); i++) {
+			//If an astranout abducted by a lander, and then setted free, it falls
 			if(astranoutList.get(i).getOnLand()==true)
 				astranoutList.get(i).move(0,1);
-			if(astranoutList.get(i).getY()==700)
+			if(astranoutList.get(i).getY()==700) //Fall to much, delete
 			{
 				removeGameObject(astranoutList.get(i));
 				astranoutList.remove(i);
@@ -435,22 +452,18 @@ public class GameEngine{
 	}
 	
 	private void enemyFire() {
+		//Select random lander
 		int randEnemy = (int)(Math.random() * landerList.size()) + 0;
+		//Select random fire time
 		int randomFire = (int)(Math.random() * 100) + 0;
+		//Create fire from the coordinates of the lander, direction to ship
 		if(randomFire == 5 && landerList.size()!=0) {
 		enemyBulletList.add(new Bullet(landerList.get(randEnemy).getX(),landerList.get(randEnemy).getY(),
 				(ship.getX()-landerList.get(randEnemy).getX())/200,(ship.getY()-landerList.get(randEnemy).getY())/200));
 		gamePane.getChildren().add(enemyBulletList.get(enemyBulletList.size()-1).getImageView());
 		}
-		for(int i=0; i<enemyBulletList.size(); i++) {
-			if(collisionManager.isCollide(ship,enemyBulletList.get(i))) {
-				ship.decreaseLife();
-			removeGameObject(enemyBulletList.get(i));
-			enemyBulletList.remove(i);
-			}
-			}
 		
-		
+		//Move the fires
 		for(int i=0; i<enemyBulletList.size(); i++) {
 		enemyBulletList.get(i).move(1, 1);
 		enemyBulletList.get(i).countTimer();
@@ -462,6 +475,8 @@ public class GameEngine{
 	}
 	
 	private void checkCollision() {
+		
+		//Check the collision of ship bullets and the landers
 		for(int i=0; i<shipBulletList.size(); i++) {
 			for(int j=0; j<landerList.size(); j++) {
 				if(collisionManager.isCollide(landerList.get(j),shipBulletList.get(i)))
@@ -477,6 +492,16 @@ public class GameEngine{
 			}
 		}
 		
+		//Check the collision of enemy bullets and the ship
+		for(int i=0; i<enemyBulletList.size(); i++) {
+			if(collisionManager.isCollide(ship,enemyBulletList.get(i))) {
+				ship.decreaseLife();
+			removeGameObject(enemyBulletList.get(i));
+			enemyBulletList.remove(i);
+			}
+			}
+		
+		//Check the collision of the astranout and the ship
 		for(int i=0; i<astranoutList.size(); i++) {
 			if(collisionManager.isCollide(astranoutList.get(i),ship))
 			if(astranoutList.get(i).getOnLand()==true) {
@@ -486,6 +511,7 @@ public class GameEngine{
 			}
 		}
 		
+		//Check the collision of the landers and the ship
 		for(int i=0; i<landerList.size(); i++) {
 			if(collisionManager.isCollide(ship, landerList.get(i))) {
 				ship.decreaseLife();
@@ -494,6 +520,7 @@ public class GameEngine{
 			}
 		}
 		
+		//Check the collision of boss1 and the ship and its bullets
 		if(boss1 != null) {
 			for(int i=0; i<shipBulletList.size(); i++) {
 			if(collisionManager.isCollide(shipBulletList.get(i), boss1)) {
@@ -508,6 +535,7 @@ public class GameEngine{
 			}
 		}
 		
+		//Check the collision of baiter and the ship and its bullets
 		if(baiter != null) {
 			for(int i=0; i<shipBulletList.size(); i++) {
 			if(collisionManager.isCollide(shipBulletList.get(i), baiter)) {
@@ -523,6 +551,7 @@ public class GameEngine{
 			}
 		}
 		
+		//Check the collision of pods and the ship and its bullets
 		if(pod1 != null) {
 			for(int i=0; i<shipBulletList.size(); i++) {
 				if(collisionManager.isCollide(shipBulletList.get(i), pod1)) {
@@ -548,6 +577,7 @@ public class GameEngine{
 			}
 		}
 		
+		//Check the collision of swarmers and the ship and its bullets
 		for(int i=0; i<shipBulletList.size(); i++) {
 			for(int j=0; j<swarmerList.size(); j++) {
 				if(collisionManager.isCollide(swarmerList.get(j),shipBulletList.get(i)))
@@ -573,7 +603,32 @@ public class GameEngine{
 		
 		enemyFire();
 		
+		for(int i=0; i<astranoutList.size(); i++) {
+			if(astranoutList.get(i).getGrabbes()) {
+				astranoutList.get(i).count();
+			}
+			
+			if(astranoutList.get(i).getGrabbes()== true && astranoutList.get(i).getCount()>200) {
+				astranoutList.get(i).setGrabbed(false);
+				gamePane.getChildren().remove(astranoutList.get(i).getImageView());
+				mutantList.add(new Mutant(astranoutList.get(i).getX(),astranoutList.get(i).getY()));
+				gamePane.getChildren().add(mutantList.get(mutantList.size()-1).getImageView());
+			}
+		}
 		
+		for(int i=0; i<mutantList.size(); i++) {
+			if(ship.getX()>mutantList.get(i).getX())
+				mutantList.get(i).setXDirection(1);
+			else
+				mutantList.get(i).setXDirection(-1);
+			
+			if(ship.getY()< mutantList.get(i).getY())
+				mutantList.get(i).setYDirection(-1);
+			else
+				mutantList.get(i).setYDirection(1);
+			
+			mutantList.get(i).move(1.5,1.5);
+		}
 		
 	}
 	
